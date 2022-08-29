06d3113ba2b71f2d73a370c4edf4056655b5476f
Adrian Pramana <adrian.astalina.pramana25@gmail.com>
2022-08-29 15:33:23 +0800

    Menambahkan UI pada login dan registration form

--- public/login.php
+++ public/login.php
@@ -56,41 +56,61 @@ if (isset($_POST["login"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Login</title>
+    <link rel="stylesheet" href="dist/output.css">
 </head>
 
 <body>
     <?php if (isset($error)) :  ?>
-        <p style="color: red; font-style:italic;">Username / Password salah!</p>
+        <div id="close-alert" class="flex w-1/2 mx-auto p-4 my-3 bg-red-100 border-t-4 border-red-500 dark:bg-red-200" role="alert">
+            <svg class="flex-shrink-0 w-5 h-5 text-red-700" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
+            </svg>
+            <div class="ml-3 text-sm font-medium text-red-700">
+                Mohon maaf <a href="#" class="font-semibold hover:text-red-800">Username atau Password</a> Anda salah!
+            </div>
+            <button type="button" class="alert-del ml-auto -mx-1.5 -my-1.5 bg-red-100 dark:bg-red-200 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 dark:hover:bg-red-300 inline-flex h-8 w-8" data-dismiss-target="#alert-border-2" aria-label="Close">
+                <span class="sr-only">Dismiss</span>
+                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                </svg>
+            </button>
+        </div>
     <?php endif; ?>
 
-    <h1>Please Login</h1>
-
+    <!-- Header -->
+    <h1 class="text-4xl mb-16 font-bold flex justify-center pt-16">Please Login</h1>
     <form action="" method="POST">
+        <div class="mb-6 flex justify-center">
 
-        <ul>
-            <li>
-                <label for="username">Username:</label>
-                <input type="text" name="username" id="username">
-            </li>
+            <label for="username" class="block mb-2 pt-2 pr-3 text-sm font-medium text-dark">Username</label>
+            <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5" name="username" id="username">
+        </div>
 
-            <li>
-                <label for="password">Password:</label>
-                <input type="password" name="password" id="password">
-            </li>
+        <div class="mb-6 flex justify-center">
+            <label for="password" class="block pt-2 pr-3 mb-2 text-sm font-medium text-dark ">Password</label>
+            <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-dark text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5">
+        </div>
 
-            <li>
-                <label for="remember">Remember</label>
-                <input type="checkbox" name="remember" id="remember">
-            </li>
-            <li>
-                <button type="submit" name="login">Login</button>
-            </li>
+        <div class="flex justify-center mb-6 pr-96">
+            <input type="checkbox" name="remember" id="remember">
 
-            <li>
-                <a href="registrasi.php">Register Now</a>
-            </li>
-        </ul>
+            <label class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300" for="remember">Remember</label>
+        </div>
+        <div class="flex justify-center mr-96">
+            <button type="submit" class="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" name="login">Login</button>
+        </div>
+        <div class="flex justify-center mt-3">
+            <p class="text-sm text-slate-700 font-semibold">If you haven't registered yet, please register first</p>
+            <a class="ml-1 text-sm font-semibold text-sky-500" href="registrasi.php">Register Now</a>
+        </div>
     </form>
+    <script>
+        const closeAlert = document.querySelector('#close-alert');
+        var alertDel = document.querySelectorAll('.alert-del');
+        alertDel.forEach((x) => x.addEventListener('click', function() {
+            x.parentElement.classList.add('hidden');
+        }));
+    </script>
 </body>
 
 </html>
--- public/registrasi.php
+++ public/registrasi.php
@@ -4,7 +4,7 @@ require 'function.php';
 if (isset($_POST["register"])) {
     if (registrasi($_POST) > 0) {
         echo "<script>
-        alert('User baru berhasil ditambahkan!')
+        alert('User baru berhasil ditambahkan!');
         </script>";
     } else {
         echo mysqli_error($conn);
@@ -21,36 +21,30 @@ if (isset($_POST["register"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Registrasi</title>
-    <style>
-        label {
-            display: block;
-        }
-    </style>
+    <link rel="stylesheet" href="dist/output.css">
 </head>
 
 <body>
 
-    <h1>Halaman Registrasi</h1>
-
-
+    <!-- Header -->
+    <h1 class="text-4xl mb-16 font-bold flex justify-center pt-16">Halaman Registrasi</h1>
     <form action="" method="POST">
-        <ul>
-            <li>
-                <label for="username">Username:</label>
-                <input type="text" name="username" id="username">
-            </li>
+        <div class="mb-6 flex justify-center">
+            <label class="block pt-2 pr-3 mb-2 text-sm font-medium text-dark" for="username">Username</label>
+            <input class="shadow-sm bg-gray-50 border border-gray-300 text-dark text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5" type="text" name="username" id="username">
+        </div>
+        <div class="mb-6 flex justify-center">
+            <label class="block pt-2 pr-3 mb-2 text-sm font-medium text-dark" for="password">Password</label>
+            <input class="shadow-sm bg-gray-50 border border-gray-300 text-dark text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5" type="password" name="password" id="password">
+        </div>
+        <div class="mb-6 flex justify-center">
+            <label class="block pt-2 pr-16 -mx-[52px] mb-2 text-sm font-medium text-dark" for="password2"> Confirm Password</label>
+            <input class="shadow-sm bg-gray-50 border border-gray-300 text-dark text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5" type="password" name="password2" id="password2">
+        </div>
 
-            <li>
-                <label for="password">Password:</label>
-                <input type="password" name="password" id="password">
-            </li>
-
-            <li>
-                <label for="password2">Konfirmasi Password :</label>
-                <input type="password" name="password2" id="password2">
-            </li>
-            <button type="submit" name="register">Register!</button>
-        </ul>
+        <div class="flex justify-center mr-[365px]">
+            <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="submit" name="register">Register!</button>
+        </div>
     </form>
 </body>
 
